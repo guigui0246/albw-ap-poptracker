f9a99d36cefbbf94aa7e3e5ddc0ee85cd56964f6
guigui0246 <guigui0246@yahoo.com>
2026-05-12 07:11:55 +0200

    Pre-update for 0.2.0

--- scripts/autotracking/setting_mapping.lua
+++ scripts/autotracking/setting_mapping.lua
@@ -1,6 +1,6 @@
 SLOT_CODES =
 {
-    
+
     -- {
     --    ["pedestal_requirement"] = 1,
     --    ["chest_size_matches_contents"] = 1,
@@ -24,16 +24,16 @@ SLOT_CODES =
     chest_size_matches_contents =
     {
         code = "csmc",
-        mapping = 
+        mapping =
         {
             [0] = 0, -- CSMC off
             [1] = 1  -- CSMC on
         }
     },
-    minigames_excluded = 
+    minigames_excluded =
     {
         code = "minigames",
-        mapping = 
+        mapping =
         {
             [0] = 0, -- Minigames on
             [1] = 1  -- Minigames off
@@ -42,7 +42,7 @@ SLOT_CODES =
     logic_mode =
     {
         code = "logic_mode",
-        mapping = 
+        mapping =
         {
             [0] = 0,   -- Normal
             [1] = 1,   -- Hard
@@ -55,7 +55,7 @@ SLOT_CODES =
     lorule_castle_requirement =
     {
         code = "lorule_castle_requirement",
-        mapping = 
+        mapping =
         {
             [0] = 7,  -- No sages
             [1] = 6,  -- 1 sage
@@ -70,7 +70,7 @@ SLOT_CODES =
     pedestal_requirement =
     {
         code = "pedestal_requirement",
-        mapping = 
+        mapping =
         {
             [0] = 1, -- Vanilla (2)
             [1] = 0  -- Standard (3)
@@ -79,7 +79,7 @@ SLOT_CODES =
     lamp_and_net_as_weapons =
     {
         code = "lamp_and_net_as_weapons",
-        mapping = 
+        mapping =
         {
             [0] = 0, -- Lamp and net not weapons
             [1] = 1  -- Lamp and net as weapons
@@ -88,7 +88,7 @@ SLOT_CODES =
     maiamai_mayhem =
     {
         code = "maiamai_mayhem",
-        mapping = 
+        mapping =
         {
             [0] = 0, -- Maiamais default
             [1] = 1  -- Maiamais shuffled
@@ -97,7 +97,7 @@ SLOT_CODES =
     nice_items =
     {
         code = "nice_items",
-        mapping = 
+        mapping =
         {
             [0] = 1,  -- Nice Items Vanilla
             [1] = 2,  -- Nice Items Shuffled (Junked)
@@ -107,7 +107,7 @@ SLOT_CODES =
     no_progression_enemies =
     {
         code = "no_progression_enemies",
-        mapping = 
+        mapping =
         {
             [0] = 0, -- Bawmbs included
             [1] = 1  -- Bawmbs excluded
@@ -116,7 +116,7 @@ SLOT_CODES =
     -- trials_required =
     -- {
     --     code = "lc_trials_door",
-    --     mapping = 
+    --     mapping =
     --     {
     --         [0] = 1,  -- Door open
     --         [1] = 0,  -- 1 trial
@@ -128,7 +128,7 @@ SLOT_CODES =
     -- open_trials_door =
     -- {
     --     code = "lc_trials_door",
-    --     mapping = 
+    --     mapping =
     --     {
     --         [0] = 0,
     --         [1] = 1
@@ -137,7 +137,7 @@ SLOT_CODES =
     dark_rooms_lampless =
     {
         code = "dark_rooms_lampless",
-        mapping = 
+        mapping =
         {
             [0] = 0, -- Lamp needed
             [1] = 1  -- No lamp needed
@@ -146,7 +146,7 @@ SLOT_CODES =
     crack_shuffle =
     {
         code = "crack_shuffle",
-        mapping = 
+        mapping =
         {
             [0] = 0,  -- Off, Cracks are not shuffled
             [1] = 1,  -- Cross World Pairs
@@ -158,7 +158,7 @@ SLOT_CODES =
     weather_vanes =
     {
         code = "weather_vanes",
-        mapping = 
+        mapping =
         {
             [0] = 0,  -- Standard
             [1] = 1,  -- Shuffled
@@ -171,7 +171,7 @@ SLOT_CODES =
     -- swordless_mode =
     -- {
     --     code = "p_sword",
-    --     mapping = 
+    --     mapping =
     --     {
     --         [0] = 1,  -- Swordful
     --         [1] = 0   -- Swordless
@@ -180,10 +180,128 @@ SLOT_CODES =
     initial_crack_state =
     {
         code = "quake",
-        mapping = 
+        mapping =
         {
             [0] = 0, -- No quake
-            [1] = 1  -- Quake
+            [1] = 1, -- Quake
+            [2] = 2  -- Progressive Merge
         }
-    }
+    },
+    -- mother_maiamai =
+    -- {
+    --     code = "maiamai_reward",
+    --     mapping =
+    --     {
+    --         [0] = 0, -- Mother Maiamai default
+    --         [1] = 1  -- Mother Maiamai shuffled
+    --     }
+    -- },
+    -- mother_maiamai_limit =
+    -- {
+    --     code = "maiamai_limit",
+    --     mapping =
+    --     {
+    --         [0] = 0,
+    --         [1] = 1,
+    --         [2] = 2,
+    --         [3] = 3,
+    --         [4] = 4,
+    --         [5] = 5,
+    --         [6] = 6,
+    --         [7] = 7,
+    --         [8] = 8,
+    --         [9] = 9,
+    --         [10] = 10,
+    --         [11] = 11,
+    --         [12] = 12,
+    --         [13] = 13,
+    --         [14] = 14,
+    --         [15] = 15,
+    --         [16] = 16,
+    --         [17] = 17,
+    --         [18] = 18,
+    --         [19] = 19,
+    --         [20] = 20,
+    --         [21] = 21,
+    --         [22] = 22,
+    --         [23] = 23,
+    --         [24] = 24,
+    --         [25] = 25,
+    --         [26] = 26,
+    --         [27] = 27,
+    --         [28] = 28,
+    --         [29] = 29,
+    --         [30] = 30,
+    --         [31] = 31,
+    --         [32] = 32,
+    --         [33] = 33,
+    --         [34] = 34,
+    --         [35] = 35,
+    --         [36] = 36,
+    --         [37] = 37,
+    --         [38] = 38,
+    --         [39] = 39,
+    --         [40] = 40,
+    --         [41] = 41,
+    --         [42] = 42,
+    --         [43] = 43,
+    --         [44] = 44,
+    --         [45] = 45,
+    --         [46] = 46,
+    --         [47] = 47,
+    --         [48] = 48,
+    --         [49] = 49,
+    --         [50] = 50,
+    --         [51] = 51,
+    --         [52] = 52,
+    --         [53] = 53,
+    --         [54] = 54,
+    --         [55] = 55,
+    --         [56] = 56,
+    --         [57] = 57,
+    --         [58] = 58,
+    --         [59] = 59,
+    --         [60] = 60,
+    --         [61] = 61,
+    --         [62] = 62,
+    --         [63] = 63,
+    --         [64] = 64,
+    --         [65] = 65,
+    --         [66] = 66,
+    --         [67] = 67,
+    --         [68] = 68,
+    --         [69] = 69,
+    --         [70] = 70,
+    --         [71] = 71,
+    --         [72] = 72,
+    --         [73] = 73,
+    --         [74] = 74,
+    --         [75] = 75,
+    --         [76] = 76,
+    --         [77] = 77,
+    --         [78] = 78,
+    --         [79] = 79,
+    --         [80] = 80,
+    --         [81] = 81,
+    --         [82] = 82,
+    --         [83] = 83,
+    --         [84] = 84,
+    --         [85] = 85,
+    --         [86] = 86,
+    --         [87] = 87,
+    --         [88] = 88,
+    --         [89] = 89,
+    --         [90] = 90,
+    --         [91] = 91,
+    --         [92] = 92,
+    --         [93] = 93,
+    --         [94] = 94,
+    --         [95] = 95,
+    --         [96] = 96,
+    --         [97] = 97,
+    --         [98] = 98,
+    --         [99] = 99,
+    --         [100] = 100
+    --     }
+    -- }
 }
